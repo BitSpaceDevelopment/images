9512a38ea0bad53443a6b486b52a8c0b182f0777
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2015-06-19 14:43:01 -0500

    Modified rbenv.sh in custom ruby 2.2.2 image to use ubuntu's rbenv (because custom images are ran as root)

--- builder/ruby/ruby_2.2.2/rbenv.sh
+++ builder/ruby/ruby_2.2.2/rbenv.sh
@@ -3,6 +3,7 @@ export PATH="/home/ubuntu/.rbenv/bin:$PATH"
 
 # set default env vars
 export RBENV_VERSION=2.2.2
+export RBENV_ROOT=/home/ubuntu/.rbenv
 export RAILS_ENV=${RAILS_ENV:-test}
 
 # initialize and set ruby version
